b0f786c8ff6ed72b130ffce5d876851ec9b5616c
Martijn Kunstman <martijn@kunstman.nl>
2024-11-26 23:04:30 +0100

    test android

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Health  : MonoBehaviour
 {
     public float maxHealth = 100f;
     public float currentHealth;
---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public string prefabTag = "Enemy"; // Zorg dat je prefab deze tag heeft
+    private Health healthScript;
+
+    public void DamageButton()
+    {
+        if (healthScript != null)
+        {
+            healthScript.TakeDamage(10); // Vermindert health met 10
+        }
+    }
+
+    public void HealButton()
+    {
+        if (healthScript != null)
+        {
+            healthScript.RestoreHealth(10); // Herstelt health met 10
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthScript == null)
+        {
+            GameObject spawnedPrefab = GameObject.FindWithTag(prefabTag);
+            if (spawnedPrefab != null)
+            {
+                healthScript = spawnedPrefab.GetComponent<Health>();
+            }
+        }
+    }
+}
